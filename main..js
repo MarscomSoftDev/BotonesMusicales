a1e853d7a677310876606ffe50f8fa4167318a99
marscom <marscompereira@gmail.com>
2024-10-31 17:22:03 -0500

    sonidos pare pan

--- main..js
+++ main..js
@@ -15,4 +15,14 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   tecla.onclick = function () {
     playSonido(idAudio);
   };
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("activa");
+      console.log(evento.code === "Space" || evento.code === "Enter");
+    }
+
+    tecla.onkeyup = function () {
+      tecla.classList.remove("activa");
+    };
+  };
 }
